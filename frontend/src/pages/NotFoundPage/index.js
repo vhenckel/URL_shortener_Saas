98dfad72f675bdffd02824e50522c65942eddb51
Vitor Henckel <vitor@henckel.com.br>
2020-11-25 14:09:56 -0300

    chore: add frontend application with React

---
+++ frontend/src/pages/NotFoundPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react' 
+
+class NotFoundPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p>404 - Page Not Found!</p>
+    )
+  }
+}
+
+export default NotFoundPage
